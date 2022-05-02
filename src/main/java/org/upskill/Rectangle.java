20cf24ae5919bf59dbaf96df1a5e0bd209b1c933
Maria Souza <mariajosefa22@yahoo.com>
2022-05-02 14:17:39 +0100

    created Rectangle Class

---
+++ src/main/java/org/upskill/Rectangle.java
@@ -0,0 +1,11 @@
+package main.java.org.upskill;
+import org.upskill.Point;
+public class Rectangle {
+    private Point upperLeft;
+    private Point bottomRight;
+
+    public Rectangle(Point upperLeft, Point bottomRight) {
+        this.upperLeft = upperLeft;
+        this.bottomRight = bottomRight;
+    }
+}
